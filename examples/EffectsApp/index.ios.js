7171855c948d3ee0525907dfde7a5c22b76468f2
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-04-11 20:53:32 +0300

    finish example

--- examples/EffectsApp/index.ios.js
+++ examples/EffectsApp/index.ios.js
@@ -7,13 +7,14 @@
 'use strict';
 
 var React = require('react-native');
+var EffectsView = require('react-native-effects-view');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  StatusBarIOS
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    StatusBarIOS
 } = React;
 
 var EffectsApp = React.createClass({
@@ -21,10 +22,61 @@ var EffectsApp = React.createClass({
         StatusBarIOS.setStyle(StatusBarIOS.Style.lightContent, true);
     },
 
+    renderVibrantExtraLight() {
+        return (
+            <View>
+                <Text style={styles.text}>Extra Light Blur</Text>
+                <Image style={styles.mask} source={require('image!ButtonRoundRect')}>
+                    <Image style={styles.camera} source={require('image!Camera')} />
+                </Image>
+            </View>
+        );
+    },
+
+    renderVibrantLight() {
+        return (
+            <View>
+                <Text style={styles.text}>Light Blur</Text>
+                <Image style={styles.mask} source={require('image!ButtonRoundRect')}>
+                    <Image style={styles.genius} source={require('image!Genius')} />
+                </Image>
+            </View>
+        );
+    },
+
+    renderVibrantDark() {
+        return (
+            <View>
+                <Text style={styles.text}>Dark Blur</Text>
+                <Image style={styles.mask} source={require('image!ButtonRoundRect')}>
+                    <Image style={styles.bitcoin} source={require('image!Bitcoin')} />
+                </Image>
+            </View>
+        );
+    },
+
     render() {
         return (
             <View style={styles.page}>
-                <Image style={styles.bg} source={require('image!Yosemite')} />
+                <Image style={styles.bg} source={require('image!Yosemite')}>
+                    <View style={styles.container}>
+                        <EffectsView
+                            style={[styles.blur, styles.extraLightBlur]}
+                            blurStyle="extraLight"
+                            vibrantContent={this.renderVibrantExtraLight()}
+                        />
+                        <EffectsView
+                            style={[styles.blur, styles.lightBlur]}
+                            blurStyle="light"
+                            vibrantContent={this.renderVibrantLight()}
+                        />
+                        <EffectsView
+                            style={[styles.blur, styles.darkBlur]}
+                            blurStyle="dark"
+                            vibrantContent={this.renderVibrantDark()}
+                        />
+                    </View>
+                </Image>
             </View>
         );
     }
@@ -33,18 +85,62 @@ var EffectsApp = React.createClass({
 var styles = StyleSheet.create({
     page: {
         flex: 1,
-        backgroundColor: 'transparent'
+        backgroundColor: 'transparent',
     },
     bg: {
         flex: 1,
         position: 'absolute',
-        bottom: 0,
+        top: 0,
         left: 0,
         right: 0,
-        top: 0,
-        justifyContent: 'center',
-        alignItems: 'center'
+        bottom: -100
     },
+    container: {
+        flex: 1
+    },
+    blur: {
+        flex: 1,
+        height: 140,
+        alignSelf: 'stretch',
+        position: 'absolute',
+        left: 0,
+        right: 0
+    },
+    extraLightBlur: {
+        bottom: 380
+    },
+    lightBlur: {
+        bottom: 240
+    },
+    darkBlur: {
+        bottom: 100
+    },
+    text: {
+        fontSize: 16,
+        marginTop: 10,
+        marginLeft: 10
+    },
+    bitcoin: {
+        width: 28,
+        height: 28
+    },
+    genius: {
+        width: 26,
+        height: 28
+    },
+    camera: {
+        width: 28,
+        height: 20
+    },
+    mask: {
+        width: 64,
+        height: 64,
+        flex: 1,
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10
+    }
 });
 
 AppRegistry.registerComponent('EffectsApp', () => EffectsApp);
